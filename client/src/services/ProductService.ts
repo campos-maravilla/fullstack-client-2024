7ec69706e1becc224062b039830df78512260ef7
campos-maravilla <antonycampos25@outlook.com>
2024-04-18 11:02:04 -0600

    introduccion-a-los-loaders-en-react-router,se hace el types de ProductSchema con id,name,price y availibility,como object,luego se crea otro type de  Product y se pone outpout typeof y se le pasa ProdcutSchema,pero como es arrary se hace otro types con el nombre de ProductsSchema

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,10 +1,11 @@
 import { safeParse } from "valibot";
-import { DraftProductSchema } from "../types";
+import { DraftProductSchema, ProductSchema, ProductsSchema } from "../types";
 import axios from "axios";
 
 type ProductData={
     [k: string]: FormDataEntryValue;
 }
+
 export async function addProduct(data:ProductData){
     try {
        const result=safeParse(DraftProductSchema,{
@@ -20,6 +21,24 @@ export async function addProduct(data:ProductData){
       
        }else{
         throw new Error('Datos no válidos')
+       }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function getProducts(){
+    try {
+        const url=`${import.meta.env.VITE_API_URL}/api/products`
+        const {data}=await axios(url)
+        //console.log(data.data)
+        const result=safeParse(ProductsSchema,data.data)
+        //console.log(data.data)
+       // console.log(result)
+       if(result.success){
+        return result.output
+       }else{
+        throw new Error('Hubo un error...')
        }
     } catch (error) {
         console.log(error)
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,6 +1,16 @@
-import { number, object, string } from "valibot";
-
+import { boolean, number, object, string,Output, array } from "valibot";
+//Output =>toma un esquema y lo convierte a un type
 export const DraftProductSchema=object({
     name:string(),
     price:number()
 })
+
+export const ProductSchema=object({
+    id:number(),
+    name:string(),
+    price:number(),
+    availability:boolean()
+})
+
+export const ProductsSchema=array(ProductSchema)
+export type Product=Output<typeof ProductSchema>
