1710a08c1e2e4ec0aa379cff91a9a1011dbc4827
campos-maravilla <antonycampos25@outlook.com>
2024-04-19 13:31:13 -0600

    creando-un-componente-de-formulario-re-utilizable

--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -2,6 +2,7 @@ import { Link,Form, useActionData, ActionFunctionArgs,redirect, LoaderFunctionAr
 import ErrorMessage from "../componentes/ErrorMessage";
 import {  getProductById, updateProduct } from "../services/ProductService";
 import { Product } from "../types";
+import ProductForm from "../componentes/ProductForm";
 
 
 
@@ -65,35 +66,9 @@ export default function EditProduct() {
       
 >
  
-    <div className="mb-4">
-        <label
-            className="text-gray-800"
-            htmlFor="name"
-        >Nombre Producto:</label>
-        <input 
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-           defaultValue={product.name}
-        />
-    </div>
-    <div className="mb-4">
-        <label
-            className="text-gray-800"
-            htmlFor="price"
-        >Precio:</label>
-        <input 
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-           defaultValue= {product.price}
-        />
-    </div>
-    
+   <ProductForm
+   product={product}
+   />
     <div className="mb-4">
         <label
             className="text-gray-800"
@@ -113,7 +88,7 @@ export default function EditProduct() {
     <input
       type="submit"
       className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-      value="Registrar Producto"
+      value="Guardar Cambios"
     />
 </Form>
     </>
